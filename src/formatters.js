cd68b6fce410352ebbf9fe63067a70e2616ae810
Konstantin Melnikov <ka.melnikov@gmail.com>
2019-07-29 13:39:22 +0800

    add number formatters

---
+++ src/formatters.js
@@ -0,0 +1,11 @@
+export const tooltipNumberFormatter = new Intl.NumberFormat('en-US', {
+  minimumFractionDigits: 3,
+});
+
+export const yAxisLabelNumberFormatter = new Intl.NumberFormat('en-US',{
+  minimumFractionDigits: 3,
+});
+
+export const crosshairLabelNumberFormatter = new Intl.NumberFormat('en-US', {
+  minimumFractionDigits: 3,
+});
